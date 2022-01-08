a856c541252827b43816098929a473242ba404d6
kjy228 <zzucepace@gmail.com>
2022-01-08 23:23:27 +0900

    빈 생명주기 콜백

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,17 +1,24 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor    // 필수값(final 객체)인 필드의 생성자를 자동으로 생성
+//@RequiredArgsConstructor    // 필수값(final 객체)인 필드의 생성자를 자동으로 생성
 //@AllArgsConstructor         // 모든 필드의 생성자를 자동으로 생성
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
---
+++ src/test/java/hello/core/lfecycle/BeanLifeCycleTest.java
@@ -0,0 +1,30 @@
+package hello.core.lfecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void LifeCycleTest(){
+        // ApplicationContext는 보통 close를 지원해주지 않는다.
+        // 그래서 하위 ConfigurableApplicationContext 인터페이스로 사용!!
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        // 호출된 결과 값이 스프링 빈에 등록됨
+        @Bean
+        public NetworkClient networkClient(){
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-kjy.dev");
+            return networkClient;
+        }
+    }
+}
